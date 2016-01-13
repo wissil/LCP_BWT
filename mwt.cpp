3142719dfa901e4c8610dad36d50acd6fbaa70c1
IvanaVanjak <ivanaa.vanjak@gmail.com>
2016-01-13 13:23:58 +0000

    Multary

---
+++ mwt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <cstring>
+#include <math.h>
+#include <sstream>
+#include <fstream>
+#include <assert.h>
+#include <time.h>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+
