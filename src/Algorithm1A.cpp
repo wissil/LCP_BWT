fcebb425d547b41bcd0ecbecfaf3bd36e8d5e6f0
IvanaVanjak <ivanaa.vanjak@gmail.com>
2016-01-14 14:17:49 +0000

    Algorithm1A added

---
+++ src/Algorithm1A.cpp
@@ -0,0 +1,35 @@
+# include "Algorithm1A.h"
+
+
+std::string Algorithm1A::uniqueCharsInInterval(index_type i, index_type j) {
+        std::string uniqueChars;
+        for(int k=0; k < alphabet->getSize(); k++) {
+            if(rank->rank(j, alphabet->getSymbols().at(k)) -
+               rank->rank(i-1, alphabet->getSymbols().at(k)) > 0) {
+                
+                uniqueChars += alphabet->getSymbols().at(k);
+            }
+        }
+        return uniqueChars;
+    }
+
+
+    
+std::vector<Interval> Algorithm1A::getIntervals(index_type i, index_type j) {
+    std::vector<Interval> intervals;
+    std::string uniqueChars = uniqueCharsInInterval(i, j);
+    
+    for(int k=0; k<uniqueChars.length(); k++) {
+        //Number of occurences of character uniqueChars[i] before interval.start index in BWT
+        index_type a = rank->rank(i-1, uniqueChars[k]);
+        //Number of occurences of character uniqueChars[i] before interval.end index in BWT
+        index_type b = rank->rank(j, uniqueChars[k]);
+        
+        symbol_type symbol = uniqueChars[k];
+        Interval newInterval;
+        newInterval.startIndex = (a + C->find(symbol)->second + 1);
+        newInterval.endIndex = (b + C->find(symbol)->second);
+        intervals.push_back(newInterval);
+    }
+    return intervals;
+}
