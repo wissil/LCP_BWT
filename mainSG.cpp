80b0f10188df2e70a168ecadd7ebb48fbc4814c8
DariaBuzic <daria.buzic@fer.hr>
2016-01-14 13:31:21 +0000

    Removed handler

--- mainSG.cpp
+++ mainSG.cpp
@@ -20,19 +20,6 @@
 using namespace std;
 using namespace sdsl;
 
-void handler(int sig) {
-    void *array[10];
-    int size;
-    
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-    
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-}
-
 int main(int argc, char* argv[]){ 
 	string file = argv[1];
 	
