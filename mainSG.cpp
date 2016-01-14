a94bc9c753c9f49b34412e01eb8a184d40b5bd14
filip <filip.kozjak@gmail.com>
2016-01-14 03:07:58 +0100

    Rank interface

--- mainSG.cpp
+++ mainSG.cpp
@@ -33,14 +33,6 @@ void handler(int sig) {
     exit(1);
 }
 
-
-void readFileToMemory(string *input, string filename);
-
-std::string generateBWT(csa_bitcompressed<> csa);
-std::map<symbol_type, occurence_type> calculateC(wt_huff<> wt,
-                                                 OrderedAlphabet alphabet);
-std::vector<Interval> getIntervals(int i, int j);
-
 int main(int argc, char* argv[]){ 
 	string file = argv[1];
 	
@@ -83,66 +75,4 @@ int main(int argc, char* argv[]){
         cout << "Memory usage: "<< usage.ru_maxrss/((float)1024) << " MB of RAM" << endl;
     }
 }
-
-//! Calculate overall number of character occurances that are
-//  strictly smaller than c, for each c in alphabet
-/*!
- *  \param wt           Generated wavelet tree from given sequence S
- *  \param alphabet     Ordered alphabet
- */
-std::map<symbol_type, occurence_type> calculateC(wt_huff<> wt,
-                                                 OrderedAlphabet alphabet) {
-    std::map<symbol_type, occurence_type> C;
-    // for each symbol c: calculate number of occurences of smaller symbols
-    for(int sym_idx=0; sym_idx<alphabet.getSize(); sym_idx++) {
-        symbol_type c = alphabet.getSymbols().at(sym_idx);
-        occurence_type n;
-        
-        if(sym_idx == 0) {
-            n = 0;
-        } else {
-            symbol_type c_prev = alphabet.getSymbols().at(sym_idx-1);
-            occurence_type n_prev = C.find(c_prev) -> second;
-            n = n_prev + wt.rank(wt.size(), c_prev);
-        }
-        C.insert(pair<symbol_type, occurence_type>(c, n));
-    }
-    return C;
-}
-
-
-//! Generate a BWT transformation string
-/*!
- *  \param csa  Suffix Array of a given sequence
- */
-std::string generateBWT(csa_bitcompressed<> csa) {
-    std::string bwt;
-    for(int i=0; i<csa.bwt.size(); i++) {
-        bwt.push_back(csa.bwt[i]);
-    }
-    return bwt;
-}
-
-
-//! Read an entire text file to memory
-/*!
- *  \param *input       Pointer to a memory block of type string
- *  \param file_name    Name of a text file being loaded to memory
- */
-void readFileToMemory(string *input, string file_name) {
-    std::string line;
-    std::ifstream file_stream;
-    file_stream.open(file_name);
-    
-    // Loading file content to memory
-    if (file_stream.is_open()) {
-        while (getline(file_stream, line)) {
-            *input += line;
-        }
-        file_stream.close();
-    }
-    else {
-        std::cout << "Unable to open file. Terminating..." << endl;
-        exit(EXIT_FAILURE);
-    }
 }
